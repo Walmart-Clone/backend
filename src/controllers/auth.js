677e39247b51c1a3a0b358b9f857641d2fce786a
AhmadT198 <alient2001@outlook.com>
2023-05-24 09:51:17 +0300

    log in using Username or email not both

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -20,7 +20,7 @@ const httpValidateAuth = async (req, res) => {
 
 const validateAuth = (req) => {
   const schema = Joi.object({
-    username: Joi.string().min(6).max(255).required(),
+    // username: Joi.string().min(6).max(255).required(),
     email: Joi.string().min(3).max(255).required().email(),
     password: Joi.string().min(5).max(1024).required(),
   });
